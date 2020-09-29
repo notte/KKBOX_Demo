41fc01aec1cef36b04aa01a519af14a4fecb79f0
Yoyo <maonome@gmail.com>
2020-09-29 11:13:37 +0800

    URL

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-	// publicPath: process.env.NODE_ENV === 'production' ?
-	// 	'/KKBOX_Demo' : '/',
-	publicPath: './',
+	publicPath: process.env.NODE_ENV === 'production' ?
+		'/KKBOX_Demo' : '/',
+	// publicPath: './',
 	devServer: {
 		open: true,
 		host: 'localhost',
